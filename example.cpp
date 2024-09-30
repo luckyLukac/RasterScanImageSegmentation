7c37f4e0e79dc7e77acd70a21eba788fa591549c
luckyLukac <73126820+luckyLukac@users.noreply.github.com>
2024-09-30 07:29:53 +0200

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,9 +1,12 @@
+#include <QImage>
 #include "Segmentation/ImageSegmentation.h"
 
 
 // Sweep-line image segmentation.
 std::vector<std::vector<Pixel>> segments;
-RawImage segmentImage;
+QImage segmentImage;
 const double epsilon = 10.0;
+const int maxDeviation = 50;
+const ImageSegmentation::Distance distanceMetric = ImageSegmentation::Distance::oneNeighbor;
 
-std::tie(segments, segmentImage) = ImageSegmentation::segmentizeImageWithSweepLine(ImageConverter::rawImageToCv(m_OriginalImage), epsilon);
+std::tie(segments, segmentImage) = ImageSegmentation::segmentizeImageWithSweepLine(ImageConverter::rawImageToCv(m_OriginalImage), epsilon, maxDeviation, distanceMetric, seed);
